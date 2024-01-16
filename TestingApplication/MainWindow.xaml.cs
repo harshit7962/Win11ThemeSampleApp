9735dae2b23bc1ba745b87908d11096be5472c27
hmishra <hmishra@microsoft.com>
2024-01-16 15:08:39 +0530

    ContentControl Testing

--- TestingApplication/MainWindow.xaml.cs
+++ TestingApplication/MainWindow.xaml.cs
@@ -17,29 +17,21 @@ namespace TestingApplication
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int counter = 0;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void RepeatButton_Click(object sender, RoutedEventArgs e)
+        private void ShowView1_Click(object sender, RoutedEventArgs e)
         {
-            // Increase the counter and update the TextBlock
-            counter++;
-            counterText.Text = counter.ToString();
+            // Set View 1 as the content of the ContentControl
+            contentControl.Content = new View1();
         }
 
-        private void ToggleButton_Checked(object sender, RoutedEventArgs e)
+        private void ShowView2_Click(object sender, RoutedEventArgs e)
         {
-            // ToggleButton is checked
-            toggleStateText.Text = "Toggle Switch is ON";
-        }
-
-        private void ToggleButton_Unchecked(object sender, RoutedEventArgs e)
-        {
-            // ToggleButton is unchecked
-            toggleStateText.Text = "Toggle Switch is OFF";
+            // Set View 2 as the content of the ContentControl
+            contentControl.Content = new View2();
         }
     }
 }
